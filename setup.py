6e4c185e84a484c9ef844b7f0fa73da70b56e0b6
dm6718 <dm6718@g.rit.edu>
2015-03-28 20:13:22 -0400

    Version 1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='RITSAR',
+      version='1.0',
+      description='Synthetic Aperture Radar (SAR) Image Processing Toolbox for Python',
+      author='Douglas Macdonald',
+      author_email='dm6718@g.rit.edu',
+      url='https://github.com/dm6718/RITSAR/',
+      packages=['ritsar'],
+     )
