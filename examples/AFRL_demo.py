c456fb5866420b0cc96716687b062d0a0a1feb68
dm6718 <dm6718@g.rit.edu>
2015-06-04 10:36:45 -0400

    Updated AFRL demo
    RVP seems to have been already accounted for in the raw data/

--- examples/AFRL_demo.py
+++ examples/AFRL_demo.py
@@ -27,14 +27,14 @@ directory = './data/AFRL/pass1'
 start_az = 1
 
 #Import phase history and create platform dictionary
-[phs, platform] = phsRead.AFRL(directory, pol, start_az, n_az = 4)
+[phs, platform] = phsRead.AFRL(directory, pol, start_az, n_az = 3)
 
 #Create image plane dictionary
 img_plane = imgTools.img_plane_dict(platform, res_factor = 1.5, upsample = True, aspect = 1.0)
 
 #Apply algorithm of choice to phase history data
 img_bp = imgTools.backprojection(phs, platform, img_plane, taylor = 43, upsample = 6)
-img_pf = imgTools.polar_format(phs, platform, img_plane, taylor = 43)
+#img_pf = imgTools.polar_format(phs, platform, img_plane, taylor = 43)
 
 #Output image
 plt.imshow(np.abs(img_bp)**0.5, cmap = cm.Greys_r)
@@ -42,8 +42,8 @@ plt.title('Backprojection')
 
 #Autofocus image
 print('autofocusing')
+#img_af, af_ph = imgTools.autoFocus(img_bp, win = 0, win_params = [300,0.8])
 img_af, af_ph = imgTools.autoFocus(img_bp, win = 0, win_params = [300,0.8])
-#img_af, af_ph = imgTools.autoFocus(img_pf, win = 0, win_params = [300,0.8])
 
 #Output autofocused image
 plt.figure()
