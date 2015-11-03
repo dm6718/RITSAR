b90bd888d628b54f89eb7a7d85ce64c84dc9f9bb
dm6718 <dm6718@g.rit.edu>
2015-11-03 08:59:37 -0500

    Update DIRSIG_demo.py

--- examples/DIRSIG_demo.py
+++ examples/DIRSIG_demo.py
@@ -15,7 +15,7 @@ from ritsar import phsRead
 from ritsar import phsTools
 from ritsar import imgTools
 
-#Define directory containing *.au2 and *.phs files
+#Define directory containing *.img files
 directory = './data/DIRSIG/'
 
 #Import phase history and create platform dictionary
